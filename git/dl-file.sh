a66f14c0f29087596fd74cc45f0253cabab3a5f7
manumuc <33813467+manumuc@users.noreply.github.com>
2019-01-07 09:48:20 +0100

    Update dl-file.sh

--- git/dl-file.sh
+++ git/dl-file.sh
@@ -1,1 +1,4 @@
-
+# DL from first layer:
+/tools# wget --no-check-certificate https://raw.githubusercontent.com/manumuc/docker/master/Dockerfile.template
+# DL from 2nd layer:
+wget --no-check-certificate https://raw.githubusercontent.com/manumuc/docker/master/basic/docker-stop-all-images
